4a11cbb45da37564e0f76ddbe3b3af1acae3ad69
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-12-01 17:48:51 +0100

    added view engines and ejs

--- app.js
+++ app.js
@@ -2,19 +2,28 @@ const express = require('express')
 
 const app = express()
 
+let ejs = require('ejs');
+
+//register view engine
+app.set('view engine', 'ejs')
+
 //Listen for requests
 app.listen(3000)
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname });
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname });
+    res.render('about');
 
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create')
 })
 
 //404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname });
+    res.status(404).render('404');
 })
